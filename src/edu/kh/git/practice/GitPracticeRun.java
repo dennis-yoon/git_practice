57b2285bfcbabcc341b02dac70515c4cbcb8df77
dennisyoon <yoonjaebeom7942@gmail.com>
2023-02-20 19:25:07 +0900

    두번째 주석 삭제 커밋

--- src/edu/kh/git/practice/GitPracticeRun.java
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -3,7 +3,7 @@ package edu.kh.git.practice;
 public class GitPracticeRun {
 
 	public static void main(String[] args) {
-		System.out.println("안녕하세요");
-		System.out.println("Git 연습 파일입니다");
+		System.out.println("안녕하세요~!!");
 	}
+
 }
